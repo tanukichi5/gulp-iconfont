b1d56386beb8053f16c5fe3bd8ba0152a4b47c91
sasaki_taku <ssktaku6172@gmail.com>
2023-02-05 00:34:54 +0900

    scssのエイリアスを設定、scssのスタイルが当たるか確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        //scssのエイリアス
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    }
+  }
+});
